492eaec03b1d84b32dbcbd9268a7ade14600f951
melih <dev.mberatsanli@gmail.com>
2025-01-12 16:51:17 +0100

    slash removed

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,10 @@ from .views import ContentView, ContentDetailView, LanguageView, ContentLanguage
 
 
 urlpatterns = [
-    path('contents/', ContentView.as_view(), name='contents'),
+    path('contents', ContentView.as_view(), name='contents'),
     path('contents/<int:content_id>', ContentDetailView.as_view(), name='content_detail'),
 
     path('contents/<int:content_id>/languages/<int:content_language_id>', ContentLanguageDetailView.as_view(), name='languages'),
 
-    path('languages/', LanguageView.as_view(), name='languages'),
+    path('languages', LanguageView.as_view(), name='languages'),
 ]
